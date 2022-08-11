28dc72555a717738bdf8ae434431819b64367b90
rohan251sh <109642798+rohan251sh@users.noreply.github.com>
2022-08-11 10:58:08 +0530

    Add files via upload

---
+++ assign/assign/Fibonacci.java
@@ -0,0 +1,20 @@
+package week1.assign;
+
+public class Fibonacci {
+
+		public static void main(String[] args) {
+			int num1=0,num2=1,num3,i,count=10;
+			System.out.println(num1+"\n"+num2);
+			for(i=2;i<count;i++)
+			{
+				num3=num1+num2;
+				System.out.println(""+num3);
+				num1=num2;
+				num2=num3;
+				
+			}
+			
+
+		}
+
+	}
---
+++ assign/assign/missingelement.java
@@ -0,0 +1,19 @@
+package week1.assign;
+
+
+
+import java.util.Arrays;
+public class missingelement {
+	public static void main(String[] args) {
+		
+
+		    int num[] ={23,45,67,32,89,22};
+		    
+			Arrays.sort(num);
+			System.out.println("Second smallest number"+num[1]);
+			
+		    
+		 
+		}
+
+	}
