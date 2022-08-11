fd18cc5222c29bc2ed73ac1f0a340ec03b412c62
rohan251sh <109642798+rohan251sh@users.noreply.github.com>
2022-08-11 10:59:49 +0530

    Add files via upload

---
+++ assign/printduplicate.java
@@ -0,0 +1,29 @@
+package week1.assign;
+
+public class printduplicate {
+
+		public static void main(String[] args) {
+			
+		
+		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		int length=arr.length;
+		int count;
+		for(int i=0;i<length;i++)
+		{
+		count=0;
+		for(int j=i+1;j<length;j++)
+		{
+			if((arr[i]==arr[j]))
+			{
+				count=count+1;
+				
+			}
+		}
+		if(count>0) {
+			System.out.println(arr[i]);
+		}
+		
+		
+	}
+		}
+	}
